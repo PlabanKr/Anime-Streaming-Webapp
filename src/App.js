326ae78f21c1f323f785cc83a5f489f1b21487bd
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2021-12-13 19:22:44 +0530

    Add style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import "./styles/App.css";
 import NavigationBar from "./components/NavigationBar";
 import SideMenu from "./components/SideMenu";
 import MainBody from "./components/MainBody";
 
 const App = () => {
   return (
-    <>
+    <div className="app-body">
       <NavigationBar />
       <section className="body">
         <SideMenu />
         <MainBody />
       </section>
-    </>
+    </div>
   );
 };
 
