cf4fe0bdc1538b9dca7749a68bae507395d98c45
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2021-12-15 20:58:47 +0530

    Complete navbar design

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,25 @@
+import "../styles/NavigationBar.css";
+
 const NavigationBar = () => {
-  return <section>Hello</section>;
+  return (
+    <nav>
+      <div className="logo">Anime Streamer</div>
+      <form className="search-form">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search"
+        />
+        <button type="submit" id="search-button">
+          <i className="fa fa-search"></i>
+        </button>
+      </form>
+      <div className="userlogo">
+        <i className="fa fa-user-circle"></i>
+      </div>
+    </nav>
+  );
 };
 
 export default NavigationBar;
