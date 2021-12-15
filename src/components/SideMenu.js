115ff9b2a74fd278776263fd77187f2c8f4c5903
Plaban Kumar Mondal <plaban.kr.mondal00@gmail.com>
2021-12-15 23:07:13 +0530

    change jsx tag

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -4,36 +4,36 @@ const SideMenu = () => {
   return (
     <div className="side-menu">
       <div className="side-menu-option-groups">
-        <a className="side-menu-option" href="#">
+        <div className="side-menu-option">
           <i className="fa fa-home"></i> Home
-        </a>
-        <a className="side-menu-option" href="#">
+        </div>
+        <div className="side-menu-option">
           <i className="fa fa-compass"></i> Discover
-        </a>
+        </div>
       </div>
       <hr />
       <div className="side-menu-option-groups">
-        <a className="side-menu-option" href="#">
+        <div className="side-menu-option">
           <i className="fa fa-bars"></i> Playlist
-        </a>
-        <a className="side-menu-option" href="#">
+        </div>
+        <div className="side-menu-option">
           <i className="fa fa-heart"></i> Favourite
-        </a>
-        <a className="side-menu-option" href="#">
+        </div>
+        <div className="side-menu-option">
           <i className="fa fa-history"></i> Watch History
-        </a>
+        </div>
       </div>
       <hr />
       <div className="side-menu-option-groups">
-        <a className="side-menu-option" href="#">
+        <div className="side-menu-option">
           <i className="fa fa-usd"></i> Subscription
-        </a>
-        <a className="side-menu-option" href="#">
+        </div>
+        <div className="side-menu-option">
           <i className="fa fa-user"></i> Profile
-        </a>
-        <a className="side-menu-option" href="#">
+        </div>
+        <div className="side-menu-option">
           <i className="fa fa-cog"></i> Settings
-        </a>
+        </div>
       </div>
     </div>
   );
